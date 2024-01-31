44ec437f96a67d1fece901bbfe59b4bb9e26429c
Emrullah Çelik <98824205+EmrullahCelk@users.noreply.github.com>
2024-01-31 17:04:39 +0100

    extract token

--- token_extract.py
+++ token_extract.py
@@ -1,22 +1,19 @@
 import os
 from google_auth_oauthlib.flow import InstalledAppFlow
+from dotenv import load_dotenv
 
-# Credential dosyanızın yolu
-credential_path = 'C:\\Users\\i7\Documents\\GitHub\\Send_Mail_With_smtplib\\credendials.json'
 
-# Token dosyanızın yolu
-token_path = 'C:\\Users\\i7\\Documents\\GitHub\\Send_Mail_With_smtplib\\token.json'
+load_dotenv()
 
-# API kapsamını belirtin (örneğin, 'https://www.googleapis.com/auth/gmail.readonly')
-scopes = ['https://www.googleapis.com/auth/gmail.send',
-          'https://www.googleapis.com/auth/gmail.readonly',
-          'https://mail.google.com/']
+credential_path = os.getenv("credentials_file_path")
+token_path = os.getenv("token_file_path")
+scopes = os.getenv("scopes")
 
-# Token dosyanız yoksa kimlik doğrulama akışını başlatın
+
 flow = InstalledAppFlow.from_client_secrets_file(credential_path, scopes)
 credentials = flow.run_local_server(port=0)
 
-# Token'ı kaydet
+# save token
 os.makedirs(os.path.dirname(token_path), exist_ok=True)
 with open(token_path, 'w') as token:
     token.write(credentials.to_json())
