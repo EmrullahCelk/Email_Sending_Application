0dac563ef1d8f48bb9dea9c19c1fb49c8e1ae7b3
Emrullah Çelik <98824205+EmrullahCelk@users.noreply.github.com>
2024-02-07 17:21:19 +0100

    update

--- send_mail_with_smtplib.py
+++ send_mail_with_smtplib.py
@@ -11,29 +11,27 @@ load_dotenv()
 
 
 def send_email(subject, body):
-    # Gönderen e-posta bilgileri
+
     from_email = getenv("sender_email")
     email_key = getenv("email_key")
     to_email = getenv("recipient_email")
 
-    # E-posta oluşturma
     msg = MIMEMultipart()
     msg['From'] = from_email
     msg['To'] = to_email
     msg['Subject'] = subject
 
-    # E-posta gövdesi
+
     msg.attach(MIMEText(body, 'plain'))
 
-    # SMTP sunucusuna bağlanma
     with smtplib.SMTP('smtp.gmail.com', 587) as server:
         server.starttls()
         server.login(from_email, email_key)
 
-        # E-posta gönderme
+
         server.sendmail(from_email, to_email, msg.as_string())
 
-# Örnek kullanım
+
 subject = "test"
 body = "test message"
 
